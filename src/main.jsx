f3609c1b82bbfeb56889e35e6f52f67236cc2bd2
Hammad Mustafa <mustafahammad745@gmal.com>
2025-08-08 17:29:18 +0500

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'antd/dist/antd.css'; // ← more compatible with Vite/Vercel builds
 
 
 createRoot(document.getElementById('root')).render(
